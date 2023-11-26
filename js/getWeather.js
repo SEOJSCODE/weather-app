f8e16f7e1e60a39e52e0c4dca6daabdf1d528370
Gennady <avenkar@gmail.com>
2023-11-26 10:50:11 +0300

    add trim

--- js/getWeather.js
+++ js/getWeather.js
@@ -3,7 +3,7 @@ import {inputValue} from "./globalConst.js";
 
 export function getWeather() {
     const serverUrl = 'https://api.openweathermap.org/data/2.5/weather'
-    const cityName = inputValue.value
+    const cityName = inputValue.value.trim()
     const apiKey = 'a21a4e21ee96247f35e1dab8b15d2fd0\n'
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`
     return fetch(`${url}`)
